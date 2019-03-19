b1cebd33598a04be2e2bb5bf5f1df8eaa9469d84
kreuzerk <kevin.kreuzer90@icloud.com>
2019-03-19 21:59:05 +0100

    feat(draganddrop): implement simple drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'drag-ng-drop';
+  items = ['Zero', 'One', 'Two', 'Three'];
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
